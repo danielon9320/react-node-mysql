55473af02a2863c6bdf0cd0ba18d27d27d450d01
danielon9320 <daniel_ls93@live.com.ar>
2021-10-14 01:20:20 -0300

    First part of Backend and DB DONE

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const db = require("./models");
+
+//Routers
+
+const postRouter = require('./routes/Posts');
+//apply middleware for router
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
+});
