c7abc8eacd555c28d3ab274430edc44d8c00fe4d
danielon9320 <daniel_ls93@live.com.ar>
2021-10-25 20:16:06 -0300

    security features added

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
-
+const { validateToken } = require("../middlewares/AuthMiddleware");
 const { sign } = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
@@ -23,16 +23,20 @@ router.post("/login", async (req, res) => {
 
   if (!user) res.json({ error: "User Doesn't Exist" });
 
-  bcrypt.compare(password, user.password).then((match) => {
+  bcrypt.compare(password, user.password).then(async (match) => {
     if (!match) res.json({ error: "Wrong Username And Password Combination" });
 
-    const accesToken = sign(
+    const accessToken = sign(
       { username: user.username, id: user.id },
       "importantsecret"
     );
-
-    res.json(accesToken);
+    res.json(accessToken);
   });
+});
+
+//evita tokens random a traves de consola
+router.get("/auth", validateToken, (req, res) => {
+  res.json(req.user);
 });
 
 module.exports = router;
