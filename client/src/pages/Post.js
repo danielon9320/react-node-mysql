9ac289c003ddcf8fdf7b5250d4b8fcd308ebe357
danielon9320 <daniel_ls93@live.com.ar>
2021-10-24 21:38:53 -0300

    bcrypt and jwt integrated

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -12,6 +12,7 @@ function Post() {
     axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
       setPostObject(response.data);
     });
+
     axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
       setComments(response.data);
     });
@@ -19,15 +20,26 @@ function Post() {
 
   const addComment = () => {
     axios
-      .post("http://localhost:3001/comments", {
-        commentBody: newComment,
-        PostId: id,
-      })
+      .post(
+        "http://localhost:3001/comments",
+        {
+          commentBody: newComment,
+          PostId: id,
+        },
+        {
+          headers: {
+            accessToken: sessionStorage.getItem("accessToken"),
+          },
+        }
+      )
       .then((response) => {
-        console.log("comment added!");
-        const commentToAdd = {commentBody: newComment}
-        setComments([...comments, commentToAdd ]);
-        setNewComment("");
+        if (response.data.error) {
+          console.log(response.data.error);
+        } else {
+          const commentToAdd = { commentBody: newComment };
+          setComments([...comments, commentToAdd]);
+          setNewComment("");
+        }
       });
   };
 
